7289bd851a4366ab678bada3f55904085c056ff5
rznamerovskiy <roman.znamerovskiy@hpe.com>
2016-08-15 15:23:39 +0300

    My third test commit

---
+++ RZnamerovskiy/src/main/java/Roma/Group/ID/App.java
@@ -0,0 +1,13 @@
+package Roma.Group.ID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
